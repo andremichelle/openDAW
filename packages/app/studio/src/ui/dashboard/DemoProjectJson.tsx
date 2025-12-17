c14c36fcbbb351fb0321c1a2d693898d05080d5b
André Michelle <andre.michelle@gmail.com>
2025-12-17 11:49:33 +0100

    Dashboard draft done

---
+++ packages/app/studio/src/ui/dashboard/DemoProjectJson.tsx
@@ -0,0 +1,13 @@
+export type DemoProjectJson = {
+    metadata: {
+        name: string
+        artist: string
+        description: string
+        tags: Array<string>
+        created: string
+        modified: string
+        coverMimeType?: string
+    }
+    hasCover: boolean
+    id: string
+}
--- packages/app/studio/src/ui/dashboard/DemoProjects.tsx
+++ packages/app/studio/src/ui/dashboard/DemoProjects.tsx
@@ -1,49 +1,57 @@
 import css from "./DemoProjects.sass?inline"
 import {Html} from "@opendaw/lib-dom"
 import {Lifecycle} from "@opendaw/lib-std"
-import {createElement, HTML} from "@opendaw/lib-jsx"
+import {Await, createElement, RouteLocation} from "@opendaw/lib-jsx"
 import {Colors} from "@opendaw/studio-enums"
 import {StudioService} from "@/service/StudioService"
+import {ThreeDots} from "@/ui/spinner/ThreeDots"
+import {DemoProjectJson} from "@/ui/dashboard/DemoProjectJson"
+import {DemoProject} from "@/ui/dashboard/DemoProject"
 
 const className = Html.adoptStyleSheet(css, "DemoProjects")
+
+type TracksList = { tracks: Array<DemoProjectJson> }
 
 type Construct = {
     lifecycle: Lifecycle
     service: StudioService
 }
 
+const listUrl = "https://api.opendaw.studio/music/list.php"
+
 export const DemoProjects = ({service}: Construct) => {
     return (
         <div className={className}>
             <h3 style={{color: Colors.orange.toString()}}>Demo Projects</h3>
-            <div className="starters">
-                {[
-                    {name: "New", click: () => service.newProject()},
-                    {name: "Sunset", click: () => service.loadTemplate("Sunset")},
-                    {name: "Breeze", click: () => service.loadTemplate("Breeze")},
-                    {name: "Shafted", click: () => service.loadTemplate("Shafted")},
-                    {name: "Seek Deeper", click: () => service.loadTemplate("SeekDeeper")},
-                    {name: "Fatso", click: () => service.loadTemplate("Fatso")},
-                    {name: "Bury Me", click: () => service.loadTemplate("BuryMe")},
-                    {
-                        name: "Bury Me (BMX Remix)",
-                        click: () => service.loadTemplate("BMX_Skyence_buryme_Remix")
-                    },
-                    {name: "Ben", click: () => service.loadTemplate("Ben")},
-                    {name: "Liquid", click: () => service.loadTemplate("BMX_LiquidDrums")},
-                    {name: "Release", click: () => service.loadTemplate("Release")},
-                    {name: "Dub Techno", click: () => service.loadTemplate("Dub-Techno")}
-                ].map(({name, click}, index) => {
-                    const svgSource = `viscious-speed/${String(index + 1)
-                        .padStart(2, "0")}.svg`
-                    return (
-                        <div onclick={click}>
-                            <HTML src={fetch(svgSource)} className="icon"/>
-                            <label>{name}</label>
-                        </div>
-                    )
-                })}
-            </div>
+            <Await factory={() => fetch(listUrl)
+                .then(res => res.json())
+                .then(res => res as TracksList)
+                .then(list => list.tracks
+                    .sort(({metadata: {modified: a}}, {metadata: {modified: b}}) => b.localeCompare(a)))}
+                   loading={() => ThreeDots()}
+                   failure={({retry}) => <span onclick={retry}>Retry</span>}
+                   success={(tracks) => (
+                       <div className="projects">
+                           <DemoProject json={{
+                               id: "NEW",
+                               hasCover: false,
+                               metadata: {
+                                   name: "Empty",
+                                   artist: "openDAW",
+                                   description: "",
+                                   tags: ["clean slate"],
+                                   created: "",
+                                   modified: "",
+                                   coverMimeType: "svg"
+                               }
+                           }} load={() => service.newProject()}/>
+                           {tracks.map(json => (
+                               <DemoProject json={json}
+                                            load={() => RouteLocation.get()
+                                                .navigateTo(`/open-bundle/${json.id}`)}/>
+                           ))}
+                       </div>
+                   )}/>
         </div>
     )
 }
