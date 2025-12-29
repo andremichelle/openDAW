2b7d68ea3bf2e8756a5c286aedd126dc0a561187
André Michelle <andre.michelle@gmail.com>
2025-12-29 08:21:00 +0100

    spread menu

--- packages/app/studio/src/ui/header/TimeStateDisplay.tsx
+++ packages/app/studio/src/ui/header/TimeStateDisplay.tsx
@@ -1,6 +1,6 @@
 import css from "./TimeStateDisplay.sass?inline"
 import {float, Lifecycle, ObservableValue, Option, Terminator} from "@opendaw/lib-std"
-import {createElement, Group, Inject} from "@opendaw/lib-jsx"
+import {createElement, Inject} from "@opendaw/lib-jsx"
 import {StudioService} from "@/service/StudioService.ts"
 import {Html} from "@opendaw/lib-dom"
 import {Preferences, ProjectProfile} from "@opendaw/studio-core"
@@ -11,7 +11,7 @@ import {TempoControl} from "@/ui/header/TempoControl"
 import {MeterControl} from "@/ui/header/MeterControl"
 import {ContextMenu} from "@/ui/ContextMenu"
 import {MenuItem} from "@/ui/model/menu-item"
-import {IconSymbol} from "@opendaw/studio-enums"
+import {Colors, IconSymbol} from "@opendaw/studio-enums"
 
 const className = Html.adoptStyleSheet(css, "TimeStateDisplay")
 
@@ -37,28 +37,26 @@ export const TimeStateDisplay = ({lifecycle, service}: Construct) => {
     }
     lifecycle.own(projectProfileService.catchupAndSubscribe(projectProfileObserver))
     const element: HTMLElement = (
-        <div className={className}>
-            <Group onInit={element => ContextMenu.subscribe(element, collector => collector.addItems(
-                MenuItem.header({label: "Time Units", icon: IconSymbol.Time}),
-                MenuItem.default({
-                    label: "Musical Time (Bars, Beats)",
-                    checked: Preferences.values["time-display"].musical
-                }).setTriggerProcedure(() => Preferences.values["time-display"].musical =
-                    !Preferences.values["time-display"].musical),
-                MenuItem.default({
-                    label: "Absolute Time (Hours, Minutes, Seconds)",
-                    checked: Preferences.values["time-display"].absolute
-                }).setTriggerProcedure(() => Preferences.values["time-display"].absolute =
-                    !Preferences.values["time-display"].absolute),
-                MenuItem.default({
-                    label: "Fine Details (Ticks, Frames)",
-                    checked: Preferences.values["time-display"].details
-                }).setTriggerProcedure(() => Preferences.values["time-display"].details =
-                    !Preferences.values["time-display"].details)
-            ))}>
-                <MusicalUnitDisplay lifecycle={lifecycle} service={service}/>
-                <AbsoluteUnitDisplay lifecycle={lifecycle} service={service}/>
-            </Group>
+        <div className={className} onInit={element => ContextMenu.subscribe(element, collector => collector.addItems(
+            MenuItem.header({label: "Time Units", icon: IconSymbol.Time, color: Colors.green}),
+            MenuItem.default({
+                label: "Musical Time (Bars, Beats)",
+                checked: Preferences.values["time-display"].musical
+            }).setTriggerProcedure(() => Preferences.values["time-display"].musical =
+                !Preferences.values["time-display"].musical),
+            MenuItem.default({
+                label: "Absolute Time (Hours, Minutes, Seconds)",
+                checked: Preferences.values["time-display"].absolute
+            }).setTriggerProcedure(() => Preferences.values["time-display"].absolute =
+                !Preferences.values["time-display"].absolute),
+            MenuItem.default({
+                label: "Fine Details (Ticks, Frames)",
+                checked: Preferences.values["time-display"].details
+            }).setTriggerProcedure(() => Preferences.values["time-display"].details =
+                !Preferences.values["time-display"].details)
+        ))}>
+            <MusicalUnitDisplay lifecycle={lifecycle} service={service}/>
+            <AbsoluteUnitDisplay lifecycle={lifecycle} service={service}/>
             <TempoControl lifecycle={lifecycle} service={service}/>
             <TapButton service={service}/>
             <MeterControl lifecycle={lifecycle} service={service}/>
