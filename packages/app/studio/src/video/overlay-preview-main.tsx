632d046712d30aec2d3488382d4a9ae6d9c39b32
André Michelle <andre.michelle@gmail.com>
2026-01-11 13:22:13 +0100

    removes file

--- packages/app/studio/src/video/overlay-preview-main.tsx
+++
@@ -1,6 +0,0 @@
-import {Terminator} from "@opendaw/lib-std"
-import {createElement} from "@opendaw/lib-jsx"
-import {VideoOverlayPreview} from "./VideoOverlayPreview"
-
-const lifecycle = new Terminator()
-document.body.appendChild(<VideoOverlayPreview lifecycle={lifecycle}/>)
