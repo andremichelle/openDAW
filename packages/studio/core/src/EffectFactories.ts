5ace8bc3c84e6d534be9c1c48fd5ff5f83726a38
André Michelle <andre.michelle@gmail.com>
2026-01-23 12:27:44 +0100

    NeuralAmp back to dev

--- packages/studio/core/src/EffectFactories.ts
+++ packages/studio/core/src/EffectFactories.ts
@@ -21,7 +21,7 @@ import {
     StereoToolDeviceBox,
     TidalDeviceBox,
     VelocityDeviceBox,
-    ZeitgeistDeviceBox
+    ZeitgeistDeviceBox,
 } from "@opendaw/studio-boxes"
 import {IconSymbol} from "@opendaw/studio-enums"
 import {DeviceManualUrls} from "@opendaw/studio-adapters"
@@ -36,11 +36,12 @@ export namespace EffectFactories {
         manualPage: DeviceManualUrls.Arpeggio,
         separatorBefore: false,
         type: "midi",
-        create: ({boxGraph}, hostField, index) => ArpeggioDeviceBox.create(boxGraph, UUID.generate(), box => {
-            box.label.setValue("Arpeggio")
-            box.index.setValue(index)
-            box.host.refer(hostField)
-        })
+        create: ({boxGraph}, hostField, index) =>
+            ArpeggioDeviceBox.create(boxGraph, UUID.generate(), (box) => {
+                box.label.setValue("Arpeggio")
+                box.index.setValue(index)
+                box.host.refer(hostField)
+            }),
     }
 
     export const Pitch: EffectFactory = {
@@ -50,11 +51,12 @@ export namespace EffectFactories {
         manualPage: DeviceManualUrls.Pitch,
         separatorBefore: false,
         type: "midi",
-        create: ({boxGraph}, hostField, index) => PitchDeviceBox.create(boxGraph, UUID.generate(), box => {
-            box.label.setValue("Pitch")
-            box.index.setValue(index)
-            box.host.refer(hostField)
-        })
+        create: ({boxGraph}, hostField, index) =>
+            PitchDeviceBox.create(boxGraph, UUID.generate(), (box) => {
+                box.label.setValue("Pitch")
+                box.index.setValue(index)
+                box.host.refer(hostField)
+            }),
     }
 
     export const Velocity: EffectFactory = {
@@ -64,11 +66,12 @@ export namespace EffectFactories {
         manualPage: DeviceManualUrls.Velocity,
         separatorBefore: false,
         type: "midi",
-        create: ({boxGraph}, hostField, index) => VelocityDeviceBox.create(boxGraph, UUID.generate(), box => {
-            box.label.setValue("Velocity")
-            box.index.setValue(index)
-            box.host.refer(hostField)
-        })
+        create: ({boxGraph}, hostField, index) =>
+            VelocityDeviceBox.create(boxGraph, UUID.generate(), (box) => {
+                box.label.setValue("Velocity")
+                box.index.setValue(index)
+                box.host.refer(hostField)
+            }),
     }
 
     export const Zeitgeist: EffectFactory = {
@@ -78,36 +81,45 @@ export namespace EffectFactories {
         manualPage: DeviceManualUrls.Zeitgeist,
         separatorBefore: false,
         type: "midi",
-        create: ({boxGraph, rootBoxAdapter}, hostField, index): ZeitgeistDeviceBox => {
+        create: (
+            {boxGraph, rootBoxAdapter},
+            hostField,
+            index,
+        ): ZeitgeistDeviceBox => {
             const useGlobal = false // TODO First Zeitgeist should be true
             const shuffleBox = useGlobal
                 ? rootBoxAdapter.groove.box
-                : GrooveShuffleBox.create(boxGraph, UUID.generate(), box => {
-                    box.label.setValue("Shuffle")
-                    box.duration.setValue(480)
-                })
-            return ZeitgeistDeviceBox.create(boxGraph, UUID.generate(), box => {
-                box.label.setValue("Zeitgeist")
-                box.groove.refer(shuffleBox)
-                box.index.setValue(index)
-                box.host.refer(hostField)
-            })
-        }
+                : GrooveShuffleBox.create(boxGraph, UUID.generate(), (box) => {
+                      box.label.setValue("Shuffle")
+                      box.duration.setValue(480)
+                  })
+            return ZeitgeistDeviceBox.create(
+                boxGraph,
+                UUID.generate(),
+                (box) => {
+                    box.label.setValue("Zeitgeist")
+                    box.groove.refer(shuffleBox)
+                    box.index.setValue(index)
+                    box.host.refer(hostField)
+                },
+            )
+        },
     }
 
     export const StereoTool: EffectFactory = {
         defaultName: "Stereo Tool",
         defaultIcon: IconSymbol.Stereo,
-        description: "Computes a stereo transformation matrix with volume, panning, phase inversion and stereo width.",
+        description:
+            "Computes a stereo transformation matrix with volume, panning, phase inversion and stereo width.",
         manualPage: DeviceManualUrls.StereoTool,
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): StereoToolDeviceBox =>
-            StereoToolDeviceBox.create(boxGraph, UUID.generate(), box => {
+            StereoToolDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Stereo Tool")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Delay: EffectFactory = {
@@ -118,27 +130,28 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): DelayDeviceBox =>
-            DelayDeviceBox.create(boxGraph, UUID.generate(), box => {
+            DelayDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Delay")
                 box.index.setValue(index)
                 box.host.refer(hostField)
                 box.version.setValue(1)
-            })
+            }),
     }
 
     export const DattorroReverb: EffectFactory = {
         defaultName: "Dattorro Reverb",
         defaultIcon: IconSymbol.Dattorro,
-        description: "Dense algorithmic reverb based on Dattorro's design, capable of infinite decay",
+        description:
+            "Dense algorithmic reverb based on Dattorro's design, capable of infinite decay",
         manualPage: DeviceManualUrls.DattorroReverb,
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): DattorroReverbDeviceBox =>
-            DattorroReverbDeviceBox.create(boxGraph, UUID.generate(), box => {
+            DattorroReverbDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Dattorro Reverb")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Maximizer: EffectFactory = {
@@ -149,26 +162,27 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): MaximizerDeviceBox =>
-            MaximizerDeviceBox.create(boxGraph, UUID.generate(), box => {
+            MaximizerDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Maximizer")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Compressor: EffectFactory = {
         defaultName: "Compressor",
         defaultIcon: IconSymbol.Compressor,
-        description: "Reduces the dynamic range by attenuating signals above a threshold",
+        description:
+            "Reduces the dynamic range by attenuating signals above a threshold",
         manualPage: DeviceManualUrls.Compressor,
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): CompressorDeviceBox =>
-            CompressorDeviceBox.create(boxGraph, UUID.generate(), box => {
+            CompressorDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Compressor")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Gate: EffectFactory = {
@@ -179,11 +193,11 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): GateDeviceBox =>
-            GateDeviceBox.create(boxGraph, UUID.generate(), box => {
+            GateDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Gate")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Reverb: EffectFactory = {
@@ -194,12 +208,12 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): ReverbDeviceBox =>
-            ReverbDeviceBox.create(boxGraph, UUID.generate(), box => {
+            ReverbDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Reverb")
                 box.preDelay.setInitValue(0.001)
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Crusher: EffectFactory = {
@@ -210,11 +224,11 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): CrusherDeviceBox =>
-            CrusherDeviceBox.create(boxGraph, UUID.generate(), box => {
+            CrusherDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Crusher")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Fold: EffectFactory = {
@@ -225,11 +239,11 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): FoldDeviceBox =>
-            FoldDeviceBox.create(boxGraph, UUID.generate(), box => {
+            FoldDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Fold")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Tidal: EffectFactory = {
@@ -240,12 +254,12 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): TidalDeviceBox =>
-            TidalDeviceBox.create(boxGraph, UUID.generate(), box => {
+            TidalDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Tidal")
                 box.index.setValue(index)
                 box.depth.setValue(0.75)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Revamp: EffectFactory = {
@@ -256,11 +270,11 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): RevampDeviceBox =>
-            RevampDeviceBox.create(boxGraph, UUID.generate(), box => {
+            RevampDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 EffectParameterDefaults.defaultRevampDeviceBox(box)
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const NeuralAmp: EffectFactory = {
@@ -271,11 +285,11 @@ export namespace EffectFactories {
         separatorBefore: false,
         type: "audio",
         create: ({boxGraph}, hostField, index): NeuralAmpDeviceBox =>
-            NeuralAmpDeviceBox.create(boxGraph, UUID.generate(), box => {
+            NeuralAmpDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Neural Amp")
                 box.index.setValue(index)
                 box.host.refer(hostField)
-            })
+            }),
     }
 
     export const Modular: EffectFactory = {
@@ -285,44 +299,73 @@ export namespace EffectFactories {
         manualPage: DeviceManualUrls.Modular,
         separatorBefore: true,
         type: "audio",
-        create: ({boxGraph, rootBox, userEditingManager}, hostField, index): ModularDeviceBox => {
-            const moduleSetupBox = ModularBox.create(boxGraph, UUID.generate(), box => {
-                box.collection.refer(rootBox.modularSetups)
-                box.label.setValue("Modular")
-            })
-            const modularInput = ModularAudioInputBox.create(boxGraph, UUID.generate(), box => {
-                box.attributes.collection.refer(moduleSetupBox.modules)
-                box.attributes.label.setValue("Modular Input")
-                box.attributes.x.setValue(-256)
-                box.attributes.y.setValue(32)
-            })
-            const modularOutput = ModularAudioOutputBox.create(boxGraph, UUID.generate(), box => {
-                box.attributes.collection.refer(moduleSetupBox.modules)
-                box.attributes.label.setValue("Modular Output")
-                box.attributes.x.setValue(256)
-                box.attributes.y.setValue(32)
-            })
-            ModuleConnectionBox.create(boxGraph, UUID.generate(), box => {
+        create: (
+            {boxGraph, rootBox, userEditingManager},
+            hostField,
+            index,
+        ): ModularDeviceBox => {
+            const moduleSetupBox = ModularBox.create(
+                boxGraph,
+                UUID.generate(),
+                (box) => {
+                    box.collection.refer(rootBox.modularSetups)
+                    box.label.setValue("Modular")
+                },
+            )
+            const modularInput = ModularAudioInputBox.create(
+                boxGraph,
+                UUID.generate(),
+                (box) => {
+                    box.attributes.collection.refer(moduleSetupBox.modules)
+                    box.attributes.label.setValue("Modular Input")
+                    box.attributes.x.setValue(-256)
+                    box.attributes.y.setValue(32)
+                },
+            )
+            const modularOutput = ModularAudioOutputBox.create(
+                boxGraph,
+                UUID.generate(),
+                (box) => {
+                    box.attributes.collection.refer(moduleSetupBox.modules)
+                    box.attributes.label.setValue("Modular Output")
+                    box.attributes.x.setValue(256)
+                    box.attributes.y.setValue(32)
+                },
+            )
+            ModuleConnectionBox.create(boxGraph, UUID.generate(), (box) => {
                 box.collection.refer(moduleSetupBox.connections)
                 box.source.refer(modularInput.output)
                 box.target.refer(modularOutput.input)
             })
             userEditingManager.modularSystem.edit(moduleSetupBox.editing)
-            return ModularDeviceBox.create(boxGraph, UUID.generate(), box => {
+            return ModularDeviceBox.create(boxGraph, UUID.generate(), (box) => {
                 box.label.setValue("Modular")
                 box.modularSetup.refer(moduleSetupBox.device)
                 box.index.setValue(index)
                 box.host.refer(hostField)
             })
-        }
+        },
     }
 
     export const MidiNamed = {Arpeggio, Pitch, Velocity, Zeitgeist}
     export const AudioNamed = {
-        StereoTool, Compressor, Gate, Delay, Reverb, DattorroReverb, Revamp, Crusher, Fold, Tidal, Maximizer /*NeuralAmp*/
+        StereoTool,
+        Compressor,
+        Gate,
+        Delay,
+        Reverb,
+        DattorroReverb,
+        Revamp,
+        Crusher,
+        Fold,
+        Tidal,
+        Maximizer,
+        NeuralAmp,
     }
-    export const MidiList: ReadonlyArray<Readonly<EffectFactory>> = Object.values(MidiNamed)
-    export const AudioList: ReadonlyArray<Readonly<EffectFactory>> = Object.values(AudioNamed)
+    export const MidiList: ReadonlyArray<Readonly<EffectFactory>> =
+        Object.values(MidiNamed)
+    export const AudioList: ReadonlyArray<Readonly<EffectFactory>> =
+        Object.values(AudioNamed)
     export const MergedNamed = {...MidiNamed, ...AudioNamed}
     export type MidiEffectKeys = keyof typeof MidiNamed
     export type AudioEffectKeys = keyof typeof AudioNamed
