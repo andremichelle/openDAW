7c70d3eefffb2bc2ac034aafdfa61fdef5e986dc
André Michelle <andre.michelle@gmail.com>
2026-01-27 13:26:06 +0100

    more logging

--- packages/app/studio/src/ui/timeline/editors/value/ValueModifyStrategies.ts
+++ packages/app/studio/src/ui/timeline/editors/value/ValueModifyStrategies.ts
@@ -1,17 +1,17 @@
 import {Interpolation, ppqn} from "@opendaw/lib-dsp"
 import {IterableIterators, Option, unitValue} from "@opendaw/lib-std"
 import {ValueEventOwnerReader} from "@/ui/timeline/editors/EventOwnerReader.ts"
-import {UIValueEvent} from "@/ui/timeline/editors/value/UIValueEvent.ts"
+import {SelectableValueEvent} from "@opendaw/studio-adapters"
 
 export interface ValueModifyStrategy {
     showOrigin(): boolean
     snapValue(): Option<number>
-    readPosition(event: UIValueEvent): ppqn
-    readValue(event: UIValueEvent): unitValue
-    readInterpolation(event: UIValueEvent): Interpolation
+    readPosition(event: SelectableValueEvent): ppqn
+    readValue(event: SelectableValueEvent): unitValue
+    readInterpolation(event: SelectableValueEvent): Interpolation
     translateSearch(value: ppqn): ppqn
-    isVisible(event: UIValueEvent): boolean
-    iterator(searchMin: ppqn, searchMax: ppqn): IterableIterator<UIValueEvent>
+    isVisible(event: SelectableValueEvent): boolean
+    iterator(searchMin: ppqn, searchMax: ppqn): IterableIterator<SelectableValueEvent>
     readContentDuration(owner: ValueEventOwnerReader): ppqn
 }
 
@@ -19,12 +19,12 @@ export namespace ValueModifyStrategy {
     export const Identity: ValueModifyStrategy = Object.freeze({
         showOrigin: (): boolean => false,
         snapValue: (): Option<unitValue> => Option.None,
-        readPosition: (event: UIValueEvent): ppqn => event.position,
-        readValue: (event: UIValueEvent): number => event.value,
-        readInterpolation: (event: UIValueEvent): Interpolation => event.interpolation,
+        readPosition: (event: SelectableValueEvent): ppqn => event.position,
+        readValue: (event: SelectableValueEvent): number => event.value,
+        readInterpolation: (event: SelectableValueEvent): Interpolation => event.interpolation,
         translateSearch: (value: ppqn): ppqn => value,
-        isVisible: (_event: UIValueEvent): boolean => true,
-        iterator: (_searchMin: ppqn, _searchMax: ppqn): IterableIterator<UIValueEvent> => IterableIterators.empty(),
+        isVisible: (_event: SelectableValueEvent): boolean => true,
+        iterator: (_searchMin: ppqn, _searchMax: ppqn): IterableIterator<SelectableValueEvent> => IterableIterators.empty(),
         readContentDuration: (region: ValueEventOwnerReader): ppqn => region.contentDuration
     })
 }
