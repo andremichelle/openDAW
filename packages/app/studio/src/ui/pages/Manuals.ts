10f8fd0f22c79a5feb704a8c7fdefa825deefa4f
André Michelle <andre.michelle@gmail.com>
2026-02-02 14:19:22 +0100

    Remove NAM

--- packages/app/studio/src/ui/pages/Manuals.ts
+++ packages/app/studio/src/ui/pages/Manuals.ts
@@ -4,18 +4,18 @@ import {InstrumentFactories} from "@opendaw/studio-adapters"
 
 export type Manual = (
     | {
-          type: "page"
-          label: string
-          path: string
-          icon?: IconSymbol
-      }
+    type: "page"
+    label: string
+    path: string
+    icon?: IconSymbol
+}
     | {
-          type: "folder"
-          label: string
-          icon?: IconSymbol
-          files: ReadonlyArray<Manual>
-      }
-) & {separatorBefore?: boolean}
+    type: "folder"
+    label: string
+    icon?: IconSymbol
+    files: ReadonlyArray<Manual>
+}
+    ) & { separatorBefore?: boolean }
 
 export const Manuals: ReadonlyArray<Manual> = [
     {
@@ -27,33 +27,33 @@ export const Manuals: ReadonlyArray<Manual> = [
             {
                 type: "page",
                 label: "Browser Support",
-                path: "/manuals/browser-support",
+                path: "/manuals/browser-support"
             },
             {
                 type: "page",
                 label: "Cloud Backup",
-                path: "/manuals/cloud-backup",
+                path: "/manuals/cloud-backup"
             },
             {
                 type: "page",
                 label: "Firefox MIDI",
-                path: "/manuals/firefox-midi",
+                path: "/manuals/firefox-midi"
             },
             {
                 type: "page",
                 label: "Keyboard Shortcuts",
-                path: "/manuals/keyboard-shortcuts",
+                path: "/manuals/keyboard-shortcuts"
             },
             {type: "page", label: "Mixer", path: "/manuals/mixer"},
             {type: "page", label: "Permissions", path: "/manuals/permissions"},
             {
                 type: "page",
                 label: "Private File System",
-                path: "/manuals/private-file-system",
+                path: "/manuals/private-file-system"
             },
             {type: "page", label: "Recording", path: "/manuals/recording"},
-            {type: "page", label: "Shadertoy", path: "/manuals/shadertoy"},
-        ],
+            {type: "page", label: "Shadertoy", path: "/manuals/shadertoy"}
+        ]
     },
     {
         type: "folder",
@@ -67,75 +67,75 @@ export const Manuals: ReadonlyArray<Manual> = [
                         type: "page",
                         label: "Cheap Reverb",
                         path: "/manuals/devices/audio/reverb",
-                        icon: EffectFactories.Reverb.defaultIcon,
+                        icon: EffectFactories.Reverb.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Compressor",
                         path: "/manuals/devices/audio/compressor",
-                        icon: EffectFactories.Compressor.defaultIcon,
+                        icon: EffectFactories.Compressor.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Crusher",
                         path: "/manuals/devices/audio/crusher",
-                        icon: EffectFactories.Crusher.defaultIcon,
+                        icon: EffectFactories.Crusher.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Dattorro Reverb",
                         path: "/manuals/devices/audio/dattorro-reverb",
-                        icon: EffectFactories.DattorroReverb.defaultIcon,
+                        icon: EffectFactories.DattorroReverb.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Delay",
                         path: "/manuals/devices/audio/delay",
-                        icon: EffectFactories.Delay.defaultIcon,
+                        icon: EffectFactories.Delay.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Fold",
                         path: "/manuals/devices/audio/fold",
-                        icon: EffectFactories.Fold.defaultIcon,
+                        icon: EffectFactories.Fold.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Gate",
                         path: "/manuals/devices/audio/gate",
-                        icon: EffectFactories.Gate.defaultIcon,
+                        icon: EffectFactories.Gate.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Maximizer",
                         path: "/manuals/devices/audio/maximizer",
-                        icon: EffectFactories.Maximizer.defaultIcon,
+                        icon: EffectFactories.Maximizer.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Neural Amp",
                         path: "/manuals/devices/audio/neural-amp",
-                        icon: EffectFactories.AudioNamed.NeuralAmp.defaultIcon,
+                        icon: /*EffectFactories.AudioNamed.NeuralAmp.defaultIcon*/ IconSymbol.Rectangle
                     },
                     {
                         type: "page",
                         label: "Revamp",
                         path: "/manuals/devices/audio/revamp",
-                        icon: EffectFactories.Revamp.defaultIcon,
+                        icon: EffectFactories.Revamp.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Stereo Tool",
                         path: "/manuals/devices/audio/stereotool",
-                        icon: EffectFactories.StereoTool.defaultIcon,
+                        icon: EffectFactories.StereoTool.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Tidal",
                         path: "/manuals/devices/audio/tidal",
-                        icon: EffectFactories.Tidal.defaultIcon,
-                    },
-                ],
+                        icon: EffectFactories.Tidal.defaultIcon
+                    }
+                ]
             },
             {
                 type: "folder",
@@ -145,39 +145,39 @@ export const Manuals: ReadonlyArray<Manual> = [
                         type: "page",
                         label: "MIDIOutput",
                         path: "/manuals/devices/instruments/midioutput",
-                        icon: InstrumentFactories.MIDIOutput.defaultIcon,
+                        icon: InstrumentFactories.MIDIOutput.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Nano",
                         path: "/manuals/devices/instruments/nano",
-                        icon: InstrumentFactories.Nano.defaultIcon,
+                        icon: InstrumentFactories.Nano.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Playfield",
                         path: "/manuals/devices/instruments/playfield",
-                        icon: InstrumentFactories.Playfield.defaultIcon,
+                        icon: InstrumentFactories.Playfield.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Soundfont",
                         path: "/manuals/devices/instruments/soundfont",
-                        icon: InstrumentFactories.Soundfont.defaultIcon,
+                        icon: InstrumentFactories.Soundfont.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Tape",
                         path: "/manuals/devices/instruments/tape",
-                        icon: InstrumentFactories.Tape.defaultIcon,
+                        icon: InstrumentFactories.Tape.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Vaporisateur",
                         path: "/manuals/devices/instruments/vaporisateur",
-                        icon: InstrumentFactories.Vaporisateur.defaultIcon,
-                    },
-                ],
+                        icon: InstrumentFactories.Vaporisateur.defaultIcon
+                    }
+                ]
             },
             {
                 type: "folder",
@@ -187,29 +187,29 @@ export const Manuals: ReadonlyArray<Manual> = [
                         type: "page",
                         label: "Arpeggio",
                         path: "/manuals/devices/midi/arpeggio",
-                        icon: EffectFactories.Arpeggio.defaultIcon,
+                        icon: EffectFactories.Arpeggio.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Pitch",
                         path: "/manuals/devices/midi/pitch",
-                        icon: EffectFactories.Pitch.defaultIcon,
+                        icon: EffectFactories.Pitch.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Velocity",
                         path: "/manuals/devices/midi/velocity",
-                        icon: EffectFactories.Velocity.defaultIcon,
+                        icon: EffectFactories.Velocity.defaultIcon
                     },
                     {
                         type: "page",
                         label: "Zeitgeist",
                         path: "/manuals/devices/midi/zeitgeist",
-                        icon: EffectFactories.Zeitgeist.defaultIcon,
-                    },
-                ],
-            },
-        ],
+                        icon: EffectFactories.Zeitgeist.defaultIcon
+                    }
+                ]
+            }
+        ]
     },
     {
         type: "folder",
@@ -218,9 +218,9 @@ export const Manuals: ReadonlyArray<Manual> = [
             {
                 type: "page",
                 label: "How to create a device in openDAW?",
-                path: "/manuals/creating-a-device",
+                path: "/manuals/creating-a-device"
             },
-            {type: "page", label: "Tech Stack", path: "/manuals/tech-stack"},
-        ],
-    },
+            {type: "page", label: "Tech Stack", path: "/manuals/tech-stack"}
+        ]
+    }
 ]
