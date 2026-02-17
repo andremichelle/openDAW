beae5f98a5aa5f4b940f60d021d74e7a9b7cf7d3
André Michelle <andre.michelle@gmail.com>
2026-02-17 09:17:20 +0100

    early bird for 742-744

--- packages/app/studio/src/features.ts
+++ packages/app/studio/src/features.ts
@@ -4,6 +4,7 @@ export const testFeatures = async (): Promise<void> => {
     requireProperty(Promise, "withResolvers")
     requireProperty(window, "indexedDB")
     requireProperty(window, "AudioWorkletNode")
+    requireProperty(window, "SharedArrayBuffer")
     requireProperty(navigator, "storage")
     requireProperty(navigator.storage, "getDirectory")
     requireProperty(crypto, "randomUUID")
