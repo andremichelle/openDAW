4dc6a37787e5f50b9f25cd3e8d68deb41c8d2cf9
André Michelle <andre.michelle@gmail.com>
2026-03-13 19:59:07 +0100

    copy

--- packages/app/studio/src/ui/werkstatt-editor/monaco-setup.ts
+++ packages/app/studio/src/ui/werkstatt-editor/monaco-setup.ts
@@ -2,6 +2,7 @@ import "@/monaco/imports"
 import * as monaco from "monaco-editor"
 import "monaco-editor/esm/vs/language/typescript/monaco.contribution"
 import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution"
+import "monaco-editor/esm/vs/editor/contrib/clipboard/browser/clipboard"
 
 monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
     noSemanticValidation: true,
