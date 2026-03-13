48912e702b54ab6910f604325cc1e4836e76f170
André Michelle <andre.michelle@gmail.com>
2026-03-13 19:52:59 +0100

    layout

--- packages/app/studio/src/ui/devices/audio-effects/WerkstattDeviceEditor.tsx
+++ packages/app/studio/src/ui/devices/audio-effects/WerkstattDeviceEditor.tsx
@@ -1,8 +1,8 @@
 import css from "./WerkstattDeviceEditor.sass?inline"
 import defaultCode from "./werkstatt-default.txt?raw"
-import {AutomatableParameterFieldAdapter, DeviceHost, WerkstattDeviceBoxAdapter} from "@opendaw/studio-adapters"
-import {asInstanceOf, EmptyExec, int, Lifecycle, Terminable, Terminator, UUID} from "@opendaw/lib-std"
-import {createElement, Group} from "@opendaw/lib-jsx"
+import {DeviceHost, WerkstattDeviceBoxAdapter} from "@opendaw/studio-adapters"
+import {asInstanceOf, EmptyExec, Lifecycle, Terminable, Terminator, UUID} from "@opendaw/lib-std"
+import {createElement} from "@opendaw/lib-jsx"
 import {DeviceEditor} from "@/ui/devices/DeviceEditor.tsx"
 import {MenuItems} from "@/ui/devices/menu-items.ts"
 import {DevicePeakMeter} from "@/ui/devices/panel/DevicePeakMeter.tsx"
@@ -11,25 +11,16 @@ import {StudioService} from "@/service/StudioService"
 import {EffectFactories} from "@opendaw/studio-core"
 import {WerkstattCompiler} from "./WerkstattCompiler"
 import {WerkstattParameterBox} from "@opendaw/studio-boxes"
-import {ParameterLabel} from "@/ui/components/ParameterLabel"
-import {RelativeUnitValueDragging} from "@/ui/wrapper/RelativeUnitValueDragging"
+import {ControlBuilder} from "@/ui/devices/ControlBuilder"
+import {Button} from "@/ui/components/Button"
 
 const className = Html.adoptStyleSheet(css, "WerkstattDeviceEditor")
-
-const NumColumns = 3
 
 type Construct = {
     lifecycle: Lifecycle
     service: StudioService
     adapter: WerkstattDeviceBoxAdapter
     deviceHost: DeviceHost
-}
-
-type Control = {
-    lifecycle: Lifecycle
-    parameter: AutomatableParameterFieldAdapter<number>
-    name: string
-    grid: { u: int, v: int }
 }
 
 export const WerkstattDeviceEditor = ({lifecycle, service, adapter, deviceHost}: Construct) => {
@@ -52,22 +43,28 @@ export const WerkstattDeviceEditor = ({lifecycle, service, adapter, deviceHost}:
         initialCode: WerkstattCompiler.stripHeader(box.code.getValue()) || defaultCode,
         previousScreen: service.layout.screen.getValue()
     })
-    const createControl = ({lifecycle: controlLifecycle, parameter, name, grid: {u, v}}: Control) => (
-        <div className="control" style={{gridArea: `${v + 1} / ${u + 1} / ${v + 3} / ${u + 2}`}}>
-            <h3>{name}</h3>
-            <RelativeUnitValueDragging lifecycle={controlLifecycle}
-                                       editing={editing}
-                                       parameter={parameter}
-                                       supressValueFlyout={true}>
-                <ParameterLabel lifecycle={controlLifecycle}
-                                editing={editing}
-                                midiLearning={midiLearning}
-                                adapter={adapter}
-                                parameter={parameter}
-                                classList={["center"]}
-                                framed={true} standalone/>
-            </RelativeUnitValueDragging>
-        </div>
+    const knobs: HTMLElement = (<div className="knobs"/>)
+    const set = UUID.newSet<{ uuid: UUID.Bytes, lifecycle: Terminable }>(({uuid}) => uuid)
+    lifecycle.own(
+        box.parameters.pointerHub.catchupAndSubscribe({
+            onAdded: ({box: paramBox}) => {
+                const werkstattParam = asInstanceOf(paramBox, WerkstattParameterBox)
+                const parameter = adapter.parameters.parameterAt(werkstattParam.value.address)
+                const terminator = new Terminator()
+                const element = ControlBuilder.createKnob({
+                    lifecycle: terminator,
+                    editing,
+                    midiLearning,
+                    adapter,
+                    parameter
+                })
+                knobs.appendChild(element)
+                set.add({uuid: paramBox.address.uuid, lifecycle: terminator})
+                terminator.own({terminate: () => element.remove()})
+            },
+            onRemoved: ({box: {address: {uuid}}}) =>
+                set.removeByKey(uuid).lifecycle.terminate()
+        })
     )
     return (
         <DeviceEditor lifecycle={lifecycle}
@@ -76,35 +73,15 @@ export const WerkstattDeviceEditor = ({lifecycle, service, adapter, deviceHost}:
                       populateMenu={parent => MenuItems.forEffectDevice(parent, service, deviceHost, adapter)}
                       populateControls={() => (
                           <div className={className}>
-                              <Group onInit={parent => {
-                                  const set = UUID.newSet<{
-                                      uuid: UUID.Bytes,
-                                      lifecycle: Terminable
-                                  }>(({uuid}) => uuid)
-                                  lifecycle.own(
-                                      box.parameters.pointerHub.catchupAndSubscribe({
-                                          onAdded: ({box: paramBox}) => {
-                                              const werkstattParam = asInstanceOf(paramBox, WerkstattParameterBox)
-                                              const parameter: AutomatableParameterFieldAdapter<number> =
-                                                  adapter.parameters.parameterAt(werkstattParam.value.address)
-                                              const terminator = new Terminator()
-                                              const index = werkstattParam.index.getValue()
-                                              const element = createControl({
-                                                  lifecycle: terminator,
-                                                  parameter,
-                                                  name: werkstattParam.label.getValue(),
-                                                  grid: {u: index % NumColumns, v: Math.floor(index / NumColumns) * 2}
-                                              })
-                                              parent.appendChild(element)
-                                              set.add({uuid: paramBox.address.uuid, lifecycle: terminator})
-                                              terminator.own({terminate: () => element.remove()})
-                                          },
-                                          onRemoved: ({box: {address: {uuid}}}) =>
-                                              set.removeByKey(uuid).lifecycle.terminate()
-                                      })
-                                  )
-                              }}/>
-                              <button className="edit" onclick={openEditor}>Edit</button>
+                              {knobs}
+                              <Button lifecycle={lifecycle}
+                                      onClick={openEditor}
+                                      appearance={{framed: true}}
+                                      style={{
+                                          fontSize: "10px",
+                                          height: "min-content",
+                                          marginTop: "2em"
+                                      }}>Editor</Button>
                           </div>
                       )}
                       populateMeter={() => (
