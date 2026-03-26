847b1f847031bd99b3a970e0b9dd5209a2c14d64
Chaosmeister <13346344+Chaosmeister@users.noreply.github.com>
2026-03-26 15:19:54 +0100

    clear decimator on discontinuous flag and deactivation

--- packages/app/studio/src/ui/devices/audio-effects/examples/alienator.js
+++ packages/app/studio/src/ui/devices/audio-effects/examples/alienator.js
@@ -65,11 +65,6 @@ class Processor {
         const srcL = src[0], srcR = src[1]
         const outL = out[0], outR = out[1]
 
-        if (flags & 2) {
-            this.ringPhL = 0.0
-            this.ringPhR = 0.0
-        }
-
         const ringInc = this.ringHz / sampleRate * this.TWO_PI
         const ringDetune = ringInc * 1.00073
         const foldDrive = 1.0 + this.fold * 18.0
@@ -89,7 +84,23 @@ class Processor {
         const chaosBase = Math.floor(0.008 * sampleRate)
         const chaosSwing = Math.floor(0.004 * sampleRate)
         const chaosFB = chaosAmt * 0.65
-
+        
+        if (flags & 2) {
+            this.ringPhL = 0.0
+            this.ringPhR = 0.0
+            this.decimHoldL = 0.0
+            this.decimHoldR = 0.0
+            this.decimCntL = 0
+            this.decimCntR = 0
+        }
+        else if (!hasDecim)
+        {
+            this.decimHoldL = 0.0
+            this.decimHoldR = 0.0
+            this.decimCntL = 0
+            this.decimCntR = 0
+        }
+        
         for (let i = s0; i < s1; i++) {
             let xL = srcL[i]
             let xR = srcR[i]
