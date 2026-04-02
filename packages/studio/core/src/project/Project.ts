a188e91a8eed7c55d4bff691838710dc730bf3e9
André Michelle <andre.michelle@gmail.com>
2026-04-02 17:34:05 +0200

    adds #230 to dev

--- packages/studio/core/src/project/Project.ts
+++ packages/studio/core/src/project/Project.ts
@@ -251,7 +251,7 @@ export class Project implements BoxAdaptersContext, Terminable, TerminableOwner 
         }
         worklet.addEventListener("error", handler)
         worklet.addEventListener("processorerror", handler)
-        worklet.connect(worklet.context.destination)
+        worklet.connect(worklet.context.destination, 0)
         this.engine.setWorklet(worklet)
         return worklet
     }
