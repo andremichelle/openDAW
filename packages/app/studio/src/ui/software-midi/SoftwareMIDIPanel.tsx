7e3f6940482b387146d67d29b13abc49d9b8140a
nlsg <93901799+nlsg@users.noreply.github.com>
2026-08-28 08:24:30 +0200

    fix(softwareKeyboard): retain state between toggles (#294)
    * fix(softwareKeyboard): retain state between toggles
    
    * fix(softwareKeyboard): attach state to module scope of SoftwareMIDIPanel
    
    * remove unused  import of clamp
    
    * fix mis-leading commas

--- packages/app/studio/src/ui/software-midi/SoftwareMIDIPanel.tsx
+++ packages/app/studio/src/ui/software-midi/SoftwareMIDIPanel.tsx
@@ -34,6 +34,9 @@ import {NoteShortcuts, SoftwareMIDIShortcuts} from "@/ui/shortcuts/SoftwareMIDIS
 
 const className = Html.adoptStyleSheet(css, "SoftwareMIDIPanel")
 
+const octave = new DefaultObservableValue(5, {guard: (value: number): number => clamp(value, 0, 10)})
+const channel = new DefaultObservableValue(0, {guard: (value: number): number => clamp(value, 0, 15)})
+const velocity =  new DefaultObservableValue(100, {guard: (value: number): number => clamp(value, 0, 100)})
 type Construct = {
     lifecycle: Lifecycle
     service: StudioService
@@ -46,9 +49,6 @@ export const SoftwareMIDIPanel = ({lifecycle, service}: Construct) => {
         blackKeys: {width: 19, height: 24}
     })
     const {softwareMIDIInput} = MidiDevices
-    const octave = new DefaultObservableValue(5, {guard: (value: number): number => clamp(value, 0, 10)})
-    const channel = new DefaultObservableValue(0, {guard: (value: number): number => clamp(value, 0, 15)})
-    const velocity = new DefaultObservableValue(100, {guard: (value: number): number => clamp(value, 0, 100)})
     const svg: SVGElement = (<PianoRoll lifecycle={lifecycle} pianoLayout={pianoLayout}/>)
     const midiIndicator: DomElement = <Icon symbol={IconSymbol.Connected}/>
     const element: HTMLElement = <div className={className}>
